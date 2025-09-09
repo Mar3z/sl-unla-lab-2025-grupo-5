6d75fa172d1ad1f4de967a90930e0c8db9a89c89
LourdesCrespo <38224067+LourdesCrespo@users.noreply.github.com>
2025-09-09 20:46:27 -0300

    Parte A completada: ABM de personas - LourdesCrespo (#1)
    * Creando proyecto base,probando si funciona la API
    
    * Configuro SQlite y SQLAlchemy en database.py
    
    * Agrego funcion para obtener sesion de la base
    
    * Creo la tabla persona en models.py y creo las tablas definidas en base
    
    * Genero esquemas pydantic para Persona implementados,PersonaBase,PersonaCreate y Persona
    
    * Implementacion de funcion calcular_edad
    
    * Endpoint POST /personas implementado
    
    * Endpoint GET /personas implementado
    
    * Endpoint GET /personas/{id} implementado
    
    * Endpoint PUT /personas/{id} implementado
    
    * Endpoint DELETE /personas/{id} implementado
    
    * Corrigiendo error de mails y dni duplicados
    
    * Agregando validacion de fecha de nacimiento para POST de personas
    
    * PARTE A COMPLETADA: practica en la API docs, ABM de personas con modelo,endopoints y validaciones,funcionando correctamente

---
+++ main.py
@@ -0,0 +1,103 @@
+from fastapi import FastAPI, Depends,HTTPException
+from sqlalchemy.orm import Session
+from database import SessionLocal,engine,Base  #Me aseguro de que exista database.py con SessionLocal
+from models import Persona
+from schemas import PersonaCreate,Persona as PersonaSchema
+from datetime import date
+from sqlalchemy.exc import IntegrityError
+#Creo tablas
+Base.metadata.create_all(bind=engine)
+app = FastAPI()
+
+#Endpoint de prueba
+"""@app.get("/")
+def inicio():
+    return {"mensaje": "Probando si funciona la api"}"""
+
+#Funcion para obtener la sesion de la base
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+
+#Funcion para calcular edad
+def calcular_edad(fecha_nacimiento: date):
+    hoy = date.today()
+    return hoy.year - fecha_nacimiento.year - ((hoy.month, hoy.day) < (fecha_nacimiento.month, fecha_nacimiento.day))
+
+# ----- ENDPOINTS DE PERSONAS (CRUD) -----
+
+#Crear persona / POST
+
+@app.post("/personas", response_model=PersonaSchema)
+def crear_persona(persona: PersonaCreate, db: Session = Depends(get_db)):
+    #Validacion:la fecha de nacimiento no puede ser futura
+    if persona.fecha_nacimiento > date.today():
+        raise HTTPException(
+            status_code=400,
+            detail="La fecha de nacimiento no puede ser futura"
+        )
+    
+    
+    edad = calcular_edad(persona.fecha_nacimiento)
+    db_persona = Persona(
+        nombre=persona.nombre,
+        email=persona.email,
+        dni=persona.dni,
+        telefono=persona.telefono,
+        fecha_nacimiento=persona.fecha_nacimiento,
+        edad=edad,
+        habilitado=True
+    )
+    db.add(db_persona)
+    try:
+        db.commit()
+        db.refresh(db_persona)
+        return db_persona
+    except IntegrityError:
+        db.rollback()  # revertir cambios de la sesión
+        raise HTTPException(
+            status_code=400, 
+            detail="Error: DNI o email ya registrado"
+        )
+#Obtener todas las personas / GET
+
+@app.get("/personas", response_model=list[PersonaSchema])
+def listar_personas(db: Session = Depends(get_db)):
+    return db.query(Persona).all()
+
+#Obtener persona por ID / GET
+
+@app.get("/personas/{id}", response_model=PersonaSchema)
+def obtener_persona(id: int, db: Session = Depends(get_db)):
+    persona = db.query(Persona).filter(Persona.id == id).first()
+    if not persona:
+        raise HTTPException(status_code=404, detail="Persona no encontrada")
+    return persona
+
+#Actualizar personas por id / PUT
+
+@app.put("/personas/{id}", response_model=PersonaSchema)
+def actualizar_persona(id: int, datos: PersonaCreate, db: Session = Depends(get_db)):
+    persona = db.query(Persona).filter(Persona.id == id).first()
+    if not persona:
+        raise HTTPException(status_code=404, detail="Persona no encontrada")
+    for key, value in datos.dict().items():
+        setattr(persona, key, value)
+    persona.edad = calcular_edad(datos.fecha_nacimiento)
+    db.commit()
+    db.refresh(persona)
+    return persona
+
+#Eliminar personas por id/ DELETE
+
+@app.delete("/personas/{id}")
+def eliminar_persona(id: int, db: Session = Depends(get_db)):
+    persona = db.query(Persona).filter(Persona.id == id).first()
+    if not persona:
+        raise HTTPException(status_code=404, detail="Persona no encontrada")
+    db.delete(persona)
+    db.commit()
+    return {"mensaje": "Persona eliminada"}
---
+++ schemas.py
@@ -0,0 +1,20 @@
+from pydantic import BaseModel, EmailStr #Importa BaseModel para crear modelos Pydantic y EmailStr para validar emails
+from datetime import date #Importa date para manejar fechas (fecha de nacimiento)
+
+class PersonaBase(BaseModel): #clase base que define los campos obligatorios de una persona
+    nombre: str
+    email: EmailStr
+    dni: str
+    telefono: str
+    fecha_nacimiento: date
+
+class PersonaCreate(PersonaBase):#para crear personas (hereda de PersonaBase)
+    pass
+
+class Persona(PersonaBase):#para respuestas
+    id: int
+    edad: int
+    habilitado: bool
+
+    class Config:
+        orm_mode = True #SQLAlchemy me devuelve los objetos directamente
