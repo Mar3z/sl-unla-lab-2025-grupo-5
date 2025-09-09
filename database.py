83c56480899e752952565aa300f72c5ecb254a87
Lourdes Noelia Crespo Barreira <lourdescrespobarreira@gmail.com>
2025-09-09 17:16:51 -0300

    Agrego funcion para obtener sesion de la base

--- database.py
+++ database.py
@@ -1,7 +1,7 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 
-# Base de datos SQLite
+#Base de datos SQLite
 DATABASE_URL = "sqlite:///./turnos.db"
 
 engine = create_engine(DATABASE_URL, connect_args={"check_same_thread": False})
--- main.py
+++ main.py
@@ -1,7 +1,18 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Depends
+from sqlalchemy.orm import Session
+from database import SessionLocal  #Me aseguro de que exista database.py con SessionLocal
 
 app = FastAPI()
 
+#Endpoint de prueba
 @app.get("/")
 def inicio():
     return {"mensaje": "Probando si funciona la api"}
+
+#Funcion para obtener la sesion de la base
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
