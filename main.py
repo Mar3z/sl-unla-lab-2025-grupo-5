247459d705aadd467d8a173fd12261abc6d0703f
Lourdes Noelia Crespo Barreira <lourdescrespobarreira@gmail.com>
2025-09-09 17:48:03 -0300

    Implementacion de funcion calcular_edad

--- main.py
+++ main.py
@@ -1,15 +1,17 @@
-from fastapi import FastAPI, Depends
+from fastapi import FastAPI, Depends,HTTPException
 from sqlalchemy.orm import Session
-from database import SessionLocal  #Me aseguro de que exista database.py con SessionLocal
-from database import engine
-from models import Base
-
+from database import SessionLocal,engine,Base  #Me aseguro de que exista database.py con SessionLocal
+from models import Persona
+from schemas import PersonaCreate,Persona as PersonaSchema
+from datetime import date
+#Creo tablas
+Base.metadata.create_all(bind=engine)
 app = FastAPI()
 
 #Endpoint de prueba
-@app.get("/")
+"""@app.get("/")
 def inicio():
-    return {"mensaje": "Probando si funciona la api"}
+    return {"mensaje": "Probando si funciona la api"}"""
 
 #Funcion para obtener la sesion de la base
 def get_db():
@@ -19,6 +21,8 @@ def get_db():
     finally:
         db.close()
 
+#Funcion para calcular edad
+def calcular_edad(fecha_nacimiento: date):
+    hoy = date.today()
+    return hoy.year - fecha_nacimiento.year - ((hoy.month, hoy.day) < (fecha_nacimiento.month, fecha_nacimiento.day))
 
-#Creo tablas
-Base.metadata.create_all(bind=engine)
