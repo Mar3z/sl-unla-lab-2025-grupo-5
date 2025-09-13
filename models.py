d2fd98a26b6cf6616f4a620a7d79d308941d84ea
nataliaarriola <natyarriola08@gmail.com>
2025-09-13 18:30:05 -0300

    Ajuste a Persona y agrego esquemas para Turno

--- models.py
+++ models.py
@@ -1,7 +1,8 @@
 
 #Importamos los tipos de datos que vamos a usar en la tabla
 #Column para definir columnas.Integer, String, Date, Boolean (tipos de datos de las columnas)
-from sqlalchemy import Column, Integer, String, Date, Boolean
+from sqlalchemy import Column, Integer, String, Date, Boolean, ForeignKey
+from sqlalchemy.orm import relationship
 
 #Importamos la clase Base que creamos en database.py (con declarative_base())
 #Esta clase es la base de todos nuestros modelos/tablas
@@ -23,5 +24,19 @@ class Persona(Base):
     fecha_nacimiento = Column(Date)
     edad = Column(Integer)
     habilitado = Column(Boolean, default=True)
+
+    # Establece relacion con Turno
+    turnos = relationship("Turno", back_populates="persona", cascade="all, delete-orphan")
+
+    class Turno(Base):
+       __tablename__ = "turnos"
+
+    id = Column(Integer, primary_key=True, index=True)
+    fecha = Column(Date, nullable=False)         # fecha del turno
+    hora = Column(String, nullable=False)        # formato "HH:MM"
+    estado = Column(String, default="pendiente") # pendiente, cancelado, confirmado, asistido. Por defecto al crear PENDIENTE
+    persona_id = Column(Integer, ForeignKey("personas.id"), nullable=False)
+
+    persona = relationship("Persona", back_populates="turnos")
 
     
--- schemas.py
+++ schemas.py
@@ -1,11 +1,12 @@
 from pydantic import BaseModel, EmailStr #Importa BaseModel para crear modelos Pydantic y EmailStr para validar emails
 from datetime import date #Importa date para manejar fechas (fecha de nacimiento)
+from typing import Optional
 
 class PersonaBase(BaseModel): #clase base que define los campos obligatorios de una persona
     nombre: str
     email: EmailStr
-    dni: int
-    telefono: int
+    dni: str
+    telefono: str
     fecha_nacimiento: date
 
 class PersonaCreate(PersonaBase):#para crear personas (hereda de PersonaBase)
@@ -18,3 +19,25 @@ class Persona(PersonaBase):#para respuestas
 
     class Config:
         orm_mode = True #SQLAlchemy me devuelve los objetos directamente
+
+
+# Schemas para Turno
+class TurnoBase(BaseModel):
+    fecha: date
+    hora: str  # "HH:MM"
+    estado: Optional[str] = None  # opcional en entrada (en creación forzaremos 'pendiente')
+
+class TurnoCreate(TurnoBase):
+    persona_id: int
+
+class TurnoUpdate(BaseModel):
+    fecha: Optional[date] = None
+    hora: Optional[str] = None
+    estado: Optional[str] = None
+
+class Turno(TurnoBase):
+    id: int
+    persona: Persona
+
+    class Config:
+        orm_mode = True
